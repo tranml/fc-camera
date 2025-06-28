728ea6e10a6824d1f86302f5ce34219a3946f83f
TranML <the44secrets@gmail.com>
2025-06-28 08:30:55 +0700

    Camera: initial commit

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,13 @@
 import { View, Text } from "react-native";
+import { Link } from "expo-router";
 
 export default function HomeScreen() {
   return (
-    <View>
-      <Text>Camera</Text>
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Welcome to Camera App</Text>
+      <Link href="/camera" style={{ marginTop: 20, color: "blue" }}>
+        Go to Camera
+      </Link>
     </View>
   );
 }
