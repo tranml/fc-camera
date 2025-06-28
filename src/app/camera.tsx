21a91948bd389125a794a8ecfd173038c49ead65
TranML <the44secrets@gmail.com>
2025-06-28 10:58:19 +0700

    Camera: style Record button

--- src/app/camera.tsx
+++ src/app/camera.tsx
@@ -1,11 +1,13 @@
 import { useEffect } from "react";
 import { View, ActivityIndicator, StyleSheet } from "react-native";
 import { useCameraPermissions, CameraView } from "expo-camera";
+import { MaterialIcons } from "@expo/vector-icons";
+import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
 
 export default function CameraScreen() {
   const [permission, requestPermission] = useCameraPermissions();
 
-  useEffect(() => { 
+  useEffect(() => {
     if (permission && !permission.granted && permission.canAskAgain) {
       requestPermission();
     }
@@ -20,7 +22,15 @@ export default function CameraScreen() {
   }
   return (
     <View>
-      <CameraView style={styles.camera} facing="front" />
+      <CameraView style={styles.camera} facing="front">
+        <View style={styles.cameraControl}>
+          <MaterialCommunityIcons
+            name="record-circle-outline"
+            size={72}
+            color="red"
+          />
+        </View>
+      </CameraView>
     </View>
   );
 }
@@ -34,5 +44,14 @@ const styles = StyleSheet.create({
   camera: {
     width: "100%",
     height: "100%",
-  }
+  },
+  cameraControl: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: "auto",
+    paddingTop: 20,
+    paddingBottom: 20,
+    backgroundColor: "#00000099",
+  },
 });
