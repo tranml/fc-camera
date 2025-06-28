edffa1e4c29c62187991306d79c309b770361d5a
TranML <the44secrets@gmail.com>
2025-06-28 11:13:01 +0700

    Camera: take photo and view it

--- src/app/camera.tsx
+++ src/app/camera.tsx
@@ -1,16 +1,24 @@
-import { useEffect, useRef } from "react";
-import { View, ActivityIndicator, StyleSheet, Pressable } from "react-native";
+import { useEffect, useRef, useState } from "react";
+import { View, ActivityIndicator, StyleSheet, Pressable, Image } from "react-native";
 import { useCameraPermissions, CameraView } from "expo-camera";
 
 export default function CameraScreen() {
   const [permission, requestPermission] = useCameraPermissions();
   const cameraRef = useRef<CameraView>(null);
+  const [photo, setPhoto] = useState<string | null>(null);
 
   useEffect(() => {
     if (permission && !permission.granted && permission.canAskAgain) {
       requestPermission();
     }
   }, [permission]);
+
+  const takePhoto = async () => {
+    if (cameraRef.current) {
+      const photo = await cameraRef.current.takePictureAsync();
+      setPhoto(photo.uri);
+    }
+  };
 
   if (!permission?.granted) {
     return (
@@ -19,11 +27,16 @@ export default function CameraScreen() {
       </View>
     );
   }
+
+  if (photo) {
+    return <Image source={{ uri: photo }} style={styles.photo} />;
+  }
+
   return (
     <View>
       <CameraView ref={cameraRef} style={styles.camera} facing="front">
         <View style={styles.cameraControl}>
-          <Pressable style={styles.recordButton}>
+          <Pressable style={styles.recordButton} onPress={takePhoto}>
             <View style={styles.recordButtonInner} />
           </Pressable>
         </View>
@@ -64,5 +77,9 @@ const styles = StyleSheet.create({
     height: 60,
     borderRadius: 30,
     backgroundColor: "red",
+  },
+  photo: {
+    width: "100%",
+    height: "100%",
   },
 });
