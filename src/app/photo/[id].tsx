3e070d25718b6684d34ba9b45a1bbd9a1fa393dd
TranML <the44secrets@gmail.com>
2025-06-28 17:33:23 +0700

    Delete a specific photo

--- src/app/photo/[id].tsx
+++ src/app/photo/[id].tsx
@@ -1,19 +1,37 @@
-import { useLocalSearchParams, Stack } from "expo-router";
-import { View, Text, Image } from "react-native";
+import { useLocalSearchParams, Stack, router } from "expo-router";
+import { View, Text, Image, Pressable } from "react-native";
 import * as FileSystem from "expo-file-system";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
 
 export default function PhotoDetailsScreen() {
   const { id } = useLocalSearchParams<{ id: string }>();
 
-  const photo = FileSystem.documentDirectory + id;
+  const photoUri = FileSystem.documentDirectory + id;
+
+  const onDeletePhoto = async () => {
+    await FileSystem.deleteAsync(photoUri, { idempotent: true });
+    router.back();
+  };
 
   return (
     <View style={{ flex: 1 }}>
       <Image
-        source={{ uri: photo }}
+        source={{ uri: photoUri }}
         style={{ width: "100%", height: "100%" }}
       />
-      <Stack.Screen options={{ title: `Photo` }} />
+      <Stack.Screen
+        options={{
+          title: `Photo`,
+          headerRight: () => (
+            <MaterialIcons
+              name="delete"
+              size={24}
+              color="red"
+              onPress={onDeletePhoto}
+            />
+          ),
+        }}
+      />
     </View>
   );
 }
