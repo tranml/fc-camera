e12235b109c05703fcd3fc69bb95c24f1d63e4ea
Luan Tran <developedbyluan@gmail.com>
2025-06-28 09:14:03 +0700

    index: position camera icon to the bottom right corner

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,4 +1,4 @@
-import { View, Text } from "react-native";
+import { View, Text, Pressable, StyleSheet } from "react-native";
 import { Link } from "expo-router";
 import MaterialIcons from "@expo/vector-icons/MaterialIcons";
 
@@ -6,17 +6,22 @@ export default function HomeScreen() {
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Text>Welcome to Camera App</Text>
-      <Link href="/camera" style={{ marginTop: 20, color: "blue" }}>
-        <View
-          style={{
-            backgroundColor: "#007AFF",
-            padding: 14,
-            borderRadius: 50,
-          }}
-        >
-          <MaterialIcons name="photo-camera" size={24} color="white" />
-        </View>
+      <Link href="/camera" style={{ marginTop: 20, color: "blue" }} asChild>
+        <Pressable style={styles.floatingButton}>
+          <MaterialIcons name="photo-camera" size={30} color="white" />
+        </Pressable>
       </Link>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  floatingButton: {
+    backgroundColor: "#007AFF",
+    padding: 14,
+    borderRadius: 50,
+    position: "absolute",
+    bottom: 20,
+    right: 20,
+  },
+});
