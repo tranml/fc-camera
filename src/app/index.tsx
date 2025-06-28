c9eda0730ebee9ec07ff44a48d3f32c31eb7cf9d
Luan Tran <developedbyluan@gmail.com>
2025-06-28 08:44:31 +0700

    index: link to CameraScreen

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,12 +1,21 @@
 import { View, Text } from "react-native";
 import { Link } from "expo-router";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
 
 export default function HomeScreen() {
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Text>Welcome to Camera App</Text>
       <Link href="/camera" style={{ marginTop: 20, color: "blue" }}>
-        Go to Camera
+        <View
+          style={{
+            backgroundColor: "#007AFF",
+            padding: 14,
+            borderRadius: 50,
+          }}
+        >
+          <MaterialIcons name="photo-camera" size={24} color="white" />
+        </View>
       </Link>
     </View>
   );
